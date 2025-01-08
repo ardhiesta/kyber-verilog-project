99c7d87cf3d90e2086749048cf46ae2d04b6696f
ardhiesta <ardhi.mail@gmail.com>
2025-01-08 15:15:58 +0800

    add new example

--- for_development_only/source_demo/display_values.v
+++ for_development_only/source_demo/display_values.v
@@ -1,7 +1,7 @@
 module display_values;
     reg [31:0] a;
     reg [7:0] b;
-    reg [575:0] c;
+    reg [575:0] c, d, e;
 
     initial begin
         // Initialize values
@@ -11,9 +11,11 @@ module display_values;
         $display("a = %h", a);
         b = a[7:0];
         $display("b = %h", b);
-        // c = { 8'h90, 8'h06, 552'h0, 8'h80 };
-        c = 576'h90abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef111111110600000000000000;
-        $display("c = %h", c[575:32]);
+        c = { 8'h90, 8'h06, 552'h0, 8'h80 };
+        d = 576'h90abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef1111111190abcdef111111110600000000000000;
+        $display("d = %h", d[575:32]);
+        e = 512'h56ac4f6845a451dac3e8886f97f7024b64b1b1e9c5181c059b5755b9a6042be653a2a0d5d56a9e1e774be5c9312f48b4798019345beae2ffcc63554a3c69862e;
+        $display("e = %h", e);
 
         // Finish simulation
         $finish;
